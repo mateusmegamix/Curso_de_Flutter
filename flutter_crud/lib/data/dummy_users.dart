8b11c229f816bc022649987128ae7d8f10a3ee82
Mateusmegamix <39318912+mateusmegamix@users.noreply.github.com>
2020-08-17 16:27:42 -0300

    Add files via upload

---
+++ flutter_crud/lib/data/dummy_users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1', name: 'Maria', email: 'maria@alunos.com.br', avatarUrl: ''),
+  '2': const User(
+      id: '2', name: 'Maria', email: 'mateus@alunos.com.br', avatarUrl: ''),
+  '3': const User(
+      id: '3', name: 'Maria', email: 'elaine@alunos.com.br', avatarUrl: ''),
+};
